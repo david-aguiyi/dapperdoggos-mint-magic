a9c10915f4e841f272752936a4f61e54e8b75778
David Aguiyi <davidaguiyi9@gmail.com>
2025-10-10 17:20:10 +0100

    Simplify Vite config to fix __DEFINES__ error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -24,14 +24,11 @@ export default defineConfig(({ mode }) => ({
     },
     plugins: [
         react(),
-        mode === "development" && componentTagger()
+        // mode === "development" && componentTagger()
     ].filter(Boolean),
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
-    },
-    define: {
-        global: 'globalThis',
     },
 }));
